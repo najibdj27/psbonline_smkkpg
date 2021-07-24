0fc386b746e6eafe3a57969aabb49e03d9ce74da
najibdj27 <najib.djulfikar@gmail.com>
2021-07-24 21:12:17 +0700

    Modify margin logo navbar

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-dark bg-dark fixed-top">
     <div class="d-flex">
         <a class="navbar-brand ms-5" href="/">
-            <img src="{{asset('Styles/IMG/logo kpg.png')}}" width="30" height="30" class="d-inline-block align-top" alt="">
+            <img src="{{asset('Styles/IMG/logo kpg.png')}}" class="mb-2 me-1" width="30" height="30" class="d-inline-block align-top" alt="">
             SMK Komunika Prestasi Gentur
         </a>
     </div>
