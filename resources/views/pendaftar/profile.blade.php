c464ed7c59ee6cc82288d091df79a81d6fd3d293
najibdj27 <najib.djulfikar@gmail.com>
2022-01-09 13:51:25 +0700

    Fix auto open modal in edit data identitas

--- resources/views/pendaftar/profile.blade.php
+++ resources/views/pendaftar/profile.blade.php
@@ -65,7 +65,7 @@
                     
                 </div>
                 <div class="d-grid gap-2 col-6 mx-auto">
-                    <button class="btn btn-outline-primary" type="button" data-bs-toggle="modal" data-bs-target="#modalIdentitas">
+                    <button class="btn btn-outline-primary" id="modalIdentitasTrigger" type="button" data-bs-toggle="modal" data-bs-target="#modalIdentitas">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                             <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
@@ -1063,11 +1063,18 @@
             </div>
         </form>
 
-        
+
     </div>
 @endsection
 
 @section('additional JS')
+    @if (count($errors)>0)
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('#modalIdentitas').modal('show');
+        });
+    </script>
+    @endif
     <script src="{{ asset('Styles/JS/sidebar.js')}}"></script>
     <script src="{{ asset('Styles/JS/imagepreview.js')}}"></script>
 @endsection
