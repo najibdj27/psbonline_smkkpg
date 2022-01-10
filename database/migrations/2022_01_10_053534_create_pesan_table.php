5c08e944adb0843021ef52ac488a9c71be4d393f
najibdj27 <najib.djulfikar@gmail.com>
2022-01-10 12:48:36 +0700

    create tabel pesan in database

---
+++ database/migrations/2022_01_10_053534_create_pesan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePesanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pesan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pendaftar_id')->onDelete('cascade')->constrained('pendaftar');
+            $table->string('jenis_pesan', 50);
+            $table->string('judul', 50);
+            $table->text('konten');
+            $table->boolean('is_read');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pesan');
+    }
+}
