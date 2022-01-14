cc602ec74373d96db98c55977abdb7d471931fb7
najibdj27 <najib.djulfikar@gmail.com>
2022-01-14 16:44:08 +0700

    make table settings in database and make model setting

---
+++ database/migrations/2022_01_14_092947_create_table_settings.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableSettings extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->year('tahun_pendaftaran')->nullable();
+            $table->string('gelombang_pendaftaran')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_settings');
+    }
+}
